f4a2ad38288e102bf3fde8c1c21b7eb73c8fc353
Gabriel Stone <Gabe.Stone@ibm.com>
2025-12-10 10:42:10 -0900

    Initial commit of initialized project.

---
+++ build/premake4.lua
@@ -0,0 +1,357 @@
+-- This premake script should be used with orx-customized version of premake4.
+-- Its Hg repository can be found at https://bitbucket.org/orx/premake-stable.
+-- A copy, including binaries, can also be found in the extern/premake folder.
+
+--
+-- Globals
+--
+
+function initconfigurations ()
+    return
+    {
+        "Debug",
+        "Profile",
+        "Release",
+        "Bundle"
+    }
+end
+
+function initplatforms ()
+    if os.is ("windows")
+    or os.is ("linux") then
+        if os.is64bit () then
+            return
+            {
+                "x64",
+                "x32",
+                "web"
+            }
+        else
+            return
+            {
+                "x32",
+                "x64",
+                "web"
+            }
+        end
+    elseif os.is ("macosx") then
+        return
+        {
+            "universal64",
+            "x64",
+            "web"
+        }
+    end
+end
+
+function defaultaction (name, action)
+   if os.is (name) then
+      _ACTION = _ACTION or action
+   end
+end
+
+defaultaction ("windows", "vs2022")
+defaultaction ("linux", "gmake")
+defaultaction ("macosx", "gmake")
+
+newoption
+{
+    trigger = "to",
+    value   = "path",
+    description = "Set the output location for the generated files"
+}
+
+if os.is ("macosx") then
+    osname = "mac"
+else
+    osname = os.get()
+end
+
+destination = _OPTIONS["to"] or "./" .. osname .. "/" .. _ACTION
+copybase = path.rebase ("..", os.getcwd (), os.getcwd () .. "/" .. destination)
+
+
+--
+-- Solution: orxtest
+--
+
+solution "orxtest"
+
+    language ("C")
+
+    location (destination)
+
+    kind ("ConsoleApp")
+
+    configurations
+    {
+        initconfigurations ()
+    }
+
+    platforms
+    {
+        initplatforms ()
+    }
+
+    targetdir ("../bin")
+
+    flags
+    {
+        "NoPCH",
+        "NoManifest",
+        "FloatFast",
+        "NoNativeWChar",
+        "NoExceptions",
+        "NoIncrementalLink",
+        "NoEditAndContinue",
+        "NoMinimalRebuild",
+        "Symbols"
+    }
+
+    configuration {"not web"}
+        flags {"StaticRuntime"}
+
+    configuration {"not xcode*", "not web"}
+        includedirs {"$(ORX)/include"}
+        libdirs {"$(ORX)/lib/dynamic"}
+
+    configuration {"xcode*", "not web"}
+        includedirs {"../include"}
+        libdirs {"../lib/dynamic"}
+
+    configuration {"x32"}
+        flags {"EnableSSE2"}
+
+    configuration {"not windows"}
+        flags {"Unicode"}
+
+    configuration {"*Debug*"}
+        targetsuffix ("d")
+        defines {"__orxDEBUG__"}
+        links {"orxd"}
+
+    configuration {"*Profile*"}
+        targetsuffix ("p")
+        defines {"__orxPROFILER__"}
+        flags {"Optimize", "NoRTTI"}
+        links {"orxp"}
+
+    configuration {"*Release*"}
+        flags {"Optimize", "NoRTTI"}
+        links {"orx"}
+
+    configuration {"*Bundle*"}
+        flags {"Optimize", "NoRTTI"}
+        links {"orx"}
+
+    configuration {"windows", "*Release*", "not web"}
+        kind ("WindowedApp")
+
+    configuration {"web"}
+        targetextension ".js"
+        targetsuffix ""
+        targetdir "../bin/web"
+        buildoptions
+        {
+            "-DorxWEB_EXECUTABLE_NAME='\"orxtest.wasm\"'",
+            "-Wno-undefined-var-template",
+            "--use-port=contrib.glfw3",
+            "-pthread"
+        }
+        linkoptions
+        {
+            "--preload-file " .. copybase .. "/build/orxtest.obr@/",
+            "-sPTHREAD_POOL_SIZE=navigator.hardwareConcurrency",
+            "-sAUDIO_WORKLET=1",
+            "-sWASM_WORKERS=1",
+            "-sSTACK_SIZE=1048576",
+            "-sASYNCIFY",
+            "-sALLOW_MEMORY_GROWTH",
+            "-sFULL_ES3=1",
+            "--use-port=contrib.glfw3",
+            "-pthread",
+            "-lidbfs.js"
+        }
+        links
+        {
+            "webpdecoder",
+            "liquidfun"
+        }
+        includedirs {"$(ORX)/include"}
+        libdirs {
+            "$(ORX)/lib/static/web",
+            "$(ORX)/../extern/libwebp/lib/web",
+            "$(ORX)/../extern/LiquidFun-1.1.0/lib/web"
+        }
+
+    configuration {"web", "*Release*"}
+        links {"orx"}
+        linkoptions {"-O2"}
+
+    configuration {"web", "*Profile*"}
+        links {"orxp"}
+        linkoptions {"-O2"}
+
+    configuration {"web", "*Debug*"}
+        links {"orxd"}
+        linkoptions {"-gsource-map"}
+
+    configuration {"web", "Windows"}
+        prelinkcommands {"cd " .. copybase .. "/bin && orxtest -b ../build/orxtest.obr"}
+        postbuildcommands {"del " .. path.translate(copybase, "\\") .. "\\build\\orxtest.obr"}
+
+    configuration {"web", "not Windows"}
+        prelinkcommands {"cd " .. copybase .. "/bin && ./orxtest -b ../build/orxtest.obr"}
+        postbuildcommands {"rm " .. copybase .. "/build/orxtest.obr"}
+
+
+-- Linux
+
+    configuration {"linux", "not web"}
+        buildoptions
+        {
+            "-Wno-unused-function"
+        }
+        linkoptions {"-Wl,-rpath ./", "-Wl,--export-dynamic"}
+        links
+        {
+            "dl",
+            "m",
+            "rt"
+        }
+
+    -- This prevents an optimization bug from happening with some versions of gcc on linux
+    configuration {"linux", "not *Debug*", "not web"}
+        buildoptions {"-fschedule-insns"}
+
+
+-- Mac OS X
+
+    configuration {"macosx", "not web"}
+        buildoptions
+        {
+            "-stdlib=libc++",
+            "-gdwarf-2",
+            "-Wno-unused-function",
+            "-Wno-write-strings"
+        }
+        linkoptions
+        {
+            "-stdlib=libc++",
+            "-dead_strip"
+        }
+
+    configuration {"macosx", "not codelite", "not codeblocks", "not web"}
+        links
+        {
+            "Foundation.framework",
+            "AppKit.framework"
+        }
+
+    configuration {"macosx", "codelite or codeblocks", "not web"}
+        linkoptions
+        {
+            "-framework Foundation",
+            "-framework AppKit"
+        }
+
+    configuration {"macosx", "x32", "not web"}
+        buildoptions
+        {
+            "-mfix-and-continue"
+        }
+
+
+-- Windows
+
+    configuration {"windows", "vs*", "not web"}
+        buildoptions
+        {
+            "/MP",
+            "/EHsc"
+        }
+
+    configuration {"windows", "gmake", "x32"}
+        prebuildcommands
+        {
+            "$(eval CC := i686-w64-mingw32-gcc)",
+            "$(eval CXX := i686-w64-mingw32-g++)",
+            "$(eval AR := i686-w64-mingw32-gcc-ar)"
+        }
+
+    configuration {"windows", "gmake", "x64"}
+        prebuildcommands
+        {
+            "$(eval CC := x86_64-w64-mingw32-gcc)",
+            "$(eval CXX := x86_64-w64-mingw32-g++)",
+            "$(eval AR := x86_64-w64-mingw32-gcc-ar)"
+        }
+
+    configuration {"windows", "codelite or codeblocks", "x32"}
+        envs
+        {
+            "CC=i686-w64-mingw32-gcc",
+            "CXX=i686-w64-mingw32-g++",
+            "AR=i686-w64-mingw32-gcc-ar"
+        }
+
+    configuration {"windows", "codelite or codeblocks", "x64"}
+        envs
+        {
+            "CC=x86_64-w64-mingw32-gcc",
+            "CXX=x86_64-w64-mingw32-g++",
+            "AR=x86_64-w64-mingw32-gcc-ar"
+        }
+
+
+--
+-- Project: orxtest
+--
+
+project "orxtest"
+
+    files
+    {
+        "../src/**.c",
+        "../include/**.h",
+        "../include/**.inc",
+        "../build/premake4.lua",
+        "../data/config/**.ini"
+    }
+
+    includedirs
+    {
+        "../include/extensions",
+        "../include"
+    }
+
+    vpaths
+    {
+        ["bundle"] = {"**.inc"},
+        ["build"] = {"**premake4.lua"},
+        ["config/**"] = {"../data/config/**.ini"}
+    }
+
+    configuration {"*Bundle*", "not web"}
+        debugargs {"-b", "orxtest.obr"}
+
+
+-- Linux
+
+    configuration {"linux", "not web"}
+        postbuildcommands {"cp -f $(ORX)/lib/dynamic/liborx*.so " .. copybase .. "/bin"}
+
+
+-- Mac OS X
+
+    configuration {"macosx", "xcode*", "not web"}
+        postbuildcommands {"cp -f ../lib/dynamic/liborx*.dylib " .. copybase .. "/bin"}
+
+    configuration {"macosx", "not xcode*", "not web"}
+        postbuildcommands {"cp -f $(ORX)/lib/dynamic/liborx*.dylib " .. copybase .. "/bin"}
+
+
+-- Windows
+
+    configuration {"windows", "not web"}
+        postbuildcommands {"cmd /c copy /Y $(ORX)\\lib\\dynamic\\orx*.dll " .. path.translate(copybase, "\\") .. "\\bin"}
---
+++ src/orxtest.c
@@ -0,0 +1,116 @@
+/**
+ * @file orxtest.c
+ * @date 8-Dec-2025
+ */
+
+#include "orx.h"
+#include "orxExtensions.h"
+
+#ifdef __orxMSVC__
+
+/* Requesting high performance dedicated GPU on hybrid laptops */
+__declspec(dllexport) unsigned long NvOptimusEnablement        = 1;
+__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+
+#endif // __orxMSVC__
+
+/** Update function, it has been registered to be called every tick of the core clock
+ */
+void orxFASTCALL Update(const orxCLOCK_INFO *_pstClockInfo, void *_pContext)
+{
+  // Should quit?
+  if(orxInput_HasBeenActivated("Quit"))
+  {
+    // Send close event
+    orxEvent_SendShort(orxEVENT_TYPE_SYSTEM, orxSYSTEM_EVENT_CLOSE);
+  }
+}
+
+/** Camera Update function, it has been registered to be called every tick of the core clock, after physics & objects have been updated
+*/
+void orxFASTCALL CameraUpdate(const orxCLOCK_INFO *_pstClockInfo, void *_pContext)
+{
+  // Get MainCamera proxy object
+  orxConfig_PushSection("MainCamera");
+  orxOBJECT *pstMainCamera = orxOBJECT(orxStructure_Get(orxConfig_GetU64("ID")));
+  orxConfig_PopSection();
+  if(pstMainCamera)
+  {
+    // Update & move the camera here
+    // [...]
+
+    // Round its position
+    orxVECTOR vPosition;
+    orxObject_SetPosition(pstMainCamera, orxVector_Round(&vPosition, orxObject_GetPosition(pstMainCamera, &vPosition)));
+  }
+}
+
+/** Init function, it is called when all orx's modules have been initialized
+ */
+orxSTATUS orxFASTCALL Init()
+{
+  // Init extensions
+  InitExtensions();
+
+  // Push [Main] as the default config section
+  orxConfig_PushSection("Main");
+
+  // Create the viewports
+  for(orxS32 i = 0, iCount = orxConfig_GetListCount("ViewportList"); i < iCount; i++)
+  {
+    orxViewport_CreateFromConfig(orxConfig_GetListString("ViewportList", i));
+  }
+
+  // Register the Update & CameraUpdate functions to the core clock
+  orxClock_Register(orxClock_Get(orxCLOCK_KZ_CORE), Update, orxNULL, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_NORMAL);
+  orxClock_Register(orxClock_Get(orxCLOCK_KZ_CORE), CameraUpdate, orxNULL, orxMODULE_ID_MAIN, orxCLOCK_PRIORITY_LOWER);
+
+  // Create the scene
+  orxObject_CreateFromConfig("Scene");
+
+  // Done!
+  return orxSTATUS_SUCCESS;
+}
+
+/** Run function, it should not contain any game logic
+ */
+orxSTATUS orxFASTCALL Run()
+{
+  // Return orxSTATUS_FAILURE to instruct orx to quit
+  return orxSTATUS_SUCCESS;
+}
+
+/** Exit function, it is called before exiting from orx
+ */
+void orxFASTCALL Exit()
+{
+  // Exit from extensions
+  ExitExtensions();
+
+  // Let orx clean all our mess automatically. :)
+}
+
+/** Bootstrap function, it is called before config is initialized, allowing for early resource storage definitions
+ */
+orxSTATUS orxFASTCALL Bootstrap()
+{
+  // Bootstrap extensions
+  BootstrapExtensions();
+
+  // Return orxSTATUS_FAILURE to prevent orx from loading the default config file
+  return orxSTATUS_SUCCESS;
+}
+
+/** Main function
+ */
+int main(int argc, char **argv)
+{
+  // Set the bootstrap function to provide at least one resource storage before loading any config files
+  orxConfig_SetBootstrap(Bootstrap);
+
+  // Execute our game
+  orx_Execute(argc, argv, Init, Run, Exit);
+
+  // Done!
+  return EXIT_SUCCESS;
+}
